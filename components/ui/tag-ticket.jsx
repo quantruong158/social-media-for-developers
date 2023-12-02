41c7e853b80ad7e48d2cc94380016d1d2e2c317d
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2023-12-02 08:34:40 +0700

    fix: change to lucide react instead of svg

--- components/ui/tag-ticket.jsx
+++ components/ui/tag-ticket.jsx
@@ -2,6 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "./button"
+import { X } from "lucide-react"
 
 const TagTicket = ({tag, onClick}) => { 
     return (
@@ -10,9 +11,7 @@ const TagTicket = ({tag, onClick}) => {
         border-primary focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 ">
                 <div className="flex w-2/4 flex-col text-white text-xs font-semibold font-['Inter']">{tag}</div>
                 <Button className="flex w-1/4 flex-col" variant="ghost" size="h-5" onClick={onClick}>
-                    <svg className="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
+                    <X className="h-6"/>
                 </Button>
         </div>
     )
