4cf615132a36f4e1c43a4f8d18a3aca04ce82324
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2023-12-02 08:56:29 +0700

    feat: adjust add tag width

--- components/ui/tag-adder.jsx
+++ components/ui/tag-adder.jsx
@@ -7,14 +7,13 @@ import { PlusCircle } from "lucide-react"
 const TagAdder = (onClick ) => { 
     return (
         <>
-            <div className="flex w-40 h-8 relative m-2 items-center justify-center 
-            rounded-2xl border-2 border-input bg-amber-600 
+            <div className="flex w-[140px] h-8 relative m-1 items-center justify-center 
+            rounded-2xl border-2 bg-amber-600 
             border-primary focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 ">
                 <Button className="flex w-1/4 flex-col" variant="ghost" size="h-5" onClick={onClick}>
                     <PlusCircle className="h-5"/>
                 </Button>
-                <div className="flex w-2/4 flex-col text-white text-x font-semibold font-['Inter']">tag to add</div>
-                    
+                <div className="flex w-3/4 flex-col text-white text-x font-semibold font-['Inter']">tag to add</div>
             </div>
         </>
     )
