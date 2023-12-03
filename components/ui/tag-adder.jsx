9a680d7780142982a85e9681742fd9acf296c258
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2023-12-03 19:32:02 +0700

    feat: dropdown menu to choose, input for custom tag

--- components/ui/tag-adder.jsx
+++ components/ui/tag-adder.jsx
@@ -2,18 +2,49 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "./button"
-import { PlusCircle } from "lucide-react"
+import { ArrowDownNarrowWide, PlusCircle } from "lucide-react"
+import { DropdownMenu,
+        DropdownMenuContent,
+        DropdownMenuItem,
+        DropdownMenuTrigger
+}from '@/components/ui/dropdown-menu'
+
+import {Input} from "@/components/ui/input"
 
 const TagAdder = (onClick ) => { 
+    const inputRef = React.useRef()
     return (
         <>
-            <div className="flex w-[140px] h-8 relative m-1 items-center justify-center 
+            <div className="flex w-[200px] h-8 relative m-1 items-center justify-center 
             rounded-2xl border-2 bg-amber-600 
             border-primary focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 ">
                 <Button className="flex w-1/4 flex-col" variant="ghost" size="h-5" onClick={onClick}>
                     <PlusCircle className="h-5"/>
                 </Button>
-                <div className="flex w-3/4 flex-col text-white text-x font-semibold font-['Inter']">tag to add</div>
+                <Input
+                    ref={inputRef}
+                    placeholder='Custom tag'
+                    className='h-5 border-0 border-primary focus:outline-none'
+                    onChange>
+                </Input>
+                <DropdownMenu className="flex w-3/4 flex-col">
+                    <DropdownMenuTrigger asChild>
+                        <button className="IconButton" aria-label="Customise options">
+                        <ArrowDownNarrowWide></ArrowDownNarrowWide>
+                        </button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align='end'>
+                        <DropdownMenuItem onClick>
+                            C++
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick>
+                            Meme
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick>
+                            Java
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </>
     )
--- components/ui/tag-ticket.jsx
+++ components/ui/tag-ticket.jsx
@@ -7,7 +7,7 @@ import { X } from "lucide-react"
 const TagTicket = ({tag, onClick}) => { 
     return (
         <div className="flex w-20 h-8 relative m-2 items-center justify-center 
-        rounded-2xl border-2 border-input bg-amber-600 
+        rounded-2xl border-2 bg-amber-600 
         border-primary focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 ">
                 <div className="flex w-2/4 flex-col text-white text-xs font-semibold font-['Inter']">{tag}</div>
                 <Button className="flex w-1/4 flex-col" variant="ghost" size="h-5" onClick={onClick}>
