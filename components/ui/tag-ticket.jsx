7b25024187a89caafcd6d1c1234e0add416c53b2
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2023-12-04 15:03:26 +0700

    refactor: change handle props name

--- components/ui/tag-ticket.jsx
+++ components/ui/tag-ticket.jsx
@@ -4,13 +4,13 @@ import { cn } from "@/lib/utils"
 import { Button } from "./button"
 import { X } from "lucide-react"
 
-const TagTicket = ({tagName, clickHandle}) => { 
+const TagTicket = ({tagName, removeClickHandle}) => { 
     return (
         <div className="flex w-20 h-8 relative m-2 items-center justify-center 
         rounded-2xl border-2 bg-amber-600 
         border-primary focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 ">
                 <div className="flex w-2/4 flex-col text-white text-xs font-semibold font-['Inter']">{tagName}</div>
-                <Button className="flex w-1/4 flex-col" variant="ghost" size="h-5" onClick={e => clickHandle(tagName)}>
+                <Button className="flex w-1/4 flex-col" variant="ghost" size="h-5" onClick={e => removeClickHandle(tagName)}>
                     <X className="h-6"/>
                 </Button>
         </div>
