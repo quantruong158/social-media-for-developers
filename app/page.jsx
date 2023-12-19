096c8d4ecc4cdd8d49302afb88c1d0c5d2123100
quantruong158 <quantruong150803@gmail.com>
2023-12-19 18:25:24 +0700

    fix logic when not signed in

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import Feed from '@/components/Feed'
-import { auth, currentUser } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs'
 import { fetchFeedPosts } from '@/app/_actions/fetch-posts'
 import LoadMore from '@/components/LoadMore'
 
@@ -7,21 +7,21 @@ export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
 export default async function Home() {
-  const { userId } = auth()
   const user = await currentUser()
+  if (!user) {
+    return (
+      <main className='mt-16 flex flex-col items-center justify-center'>
+        <h2 className='text-2xl font-semibold text-primary'>
+          You&#39;re not signed in!
+        </h2>
+      </main>
+    )
+  }
   const posts = await fetchFeedPosts(user.username, 1)
   return (
     <main className='mt-16 flex flex-col items-center justify-center'>
-      {userId ? (
-        <>
-          <Feed username={user.username} posts={posts} />
-          <LoadMore username={user.username} fetchFn={fetchFeedPosts} />
-        </>
-      ) : (
-        <h2 className='text-2xl font-semibold text-primary'>
-          You&amp;re not signed in!
-        </h2>
-      )}
+      <Feed username={user.username} posts={posts} />
+      <LoadMore username={user.username} fetchFn={fetchFeedPosts} />
     </main>
   )
 }
